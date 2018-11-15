fbce22094fccec25545f54e978a5ff9015df3902
carolynmccawley <cam1010@nyu.edu>
2018-11-15 12:36:40 -0500

    Updating assignment 8

--- Assignments/Assignment8/app.js
+++ Assignments/Assignment8/app.js
@@ -1,40 +1,43 @@
 var express = require('express')
 var app = express()
 
-
 //set the template engine ejs
 app.set('view engine', 'ejs')
-
-//middlewares
 app.use(express.static('public'))
 
 
 //routes
+//show the index ejs file
 app.get('/', function(req, res){
 	res.render('index')
 })
 
 //Listen on port 3000
-server = app.listen(3000)
+server = app.listen(3000, function(){
+    console.log("running on port 3000");
+})
 
 
 
 //socket.io instantiation
 var io = require("socket.io")(server)
 
+//keeping track of users -- number and names
 var userCount = 0;
 var onlineUsers=[];
 
 
 io.on('connection', function(socket){
-    //console.log(socket.username);
     //default username
 	socket.username = "Anonymous";
-    
     //console.log(socket)
+    //increases the count by 1 to reflect new person going to website
+     userCount++;
+    //adds unique id and the correspoding username to array
+    onlineUsers.push({'id':socket.id,'username':socket.username});
 
     //changes the username associated with each person's unique id
-    socket.on('change_username', function(data){
+     socket.on('change_username', function(data){
         var index = 0;
         //finds the unique id of the person changing their username and then changes the username associated with the id
         for (var i = 0; i < onlineUsers.length; i++){
@@ -48,12 +51,6 @@ io.on('connection', function(socket){
         io.sockets.emit('userList',{userList: onlineUsers});
 
     })
-    //increases the count by 1 to reflect new person going to website
-     userCount++;
-    //console.log(socket.id);
-    //adds unique id and the correspoding username to array
-    onlineUsers.push({'id':socket.id,'username':socket.username});
-    //console.log(onlineUsers);
     //update the count when someone comes online
     io.sockets.emit('userCount',{userCount: userCount});
     //update the list of users when someone comes online
@@ -68,11 +65,7 @@ io.on('connection', function(socket){
                 index=i;
             }  
         }
-        //console.log('before deleting');
-        //console.log(onlineUsers);
         onlineUsers.splice(index);
-        //console.log('after deleting')
-        //console.log(onlineUsers);
             
         //updating count and user list
         io.sockets.emit('userCount',{userCount:userCount});
@@ -103,3 +96,4 @@ io.on('connection', function(socket){
     	socket.broadcast.emit('typing', {username : onlineUsers[userIndex].username})
     })
 })
+
